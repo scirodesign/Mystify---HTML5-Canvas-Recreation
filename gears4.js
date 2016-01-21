21a04c521c0cc0164cb45ee8667965a5cf30e79a
Andy <andy@scirodesign.com>
2016-01-21 11:20:08 -0500

    ore progress on arrays

--- gears4.js
+++ gears4.js
@@ -9,28 +9,25 @@ jQuery(document).ready(function($) {
 
   var pointsAmt = 4;
   var lineChildren = 4;
-  var degradeDistance = 10;
-  var rgb = [Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256)];
+  var degradeDistance = 20;
   var masterPointArray = [];
-
+  var childrenArray = [];
 
 
 
   for(var i=0; i<pointsAmt; i++){
     var singleLinePoint = new pointObj(_.random(0, canvas.width),_.random(0, canvas.height),lineChildren);
-    masterPointArray.push(singleLinePoint);
+    childrenArray.push(singleLinePoint);
   }
-
+  masterPointArray.push(childrenArray);
 
 
 
   function pointObj(px,py){
       this.x = px;
       this.y = py;
-      this.XhistoryArray = [];
-      this.YhistoryArray = [];
-      this.dirX = _.random(.5,.5);
-      this.dirY = _.random(.5,.5);
+      this.dirX = _.random(-2,-2);
+      this.dirY = _.random(-2,-2);
       this.lvDx = 0;
       this.lvDy = 0;
       return this;
@@ -47,33 +44,41 @@ jQuery(document).ready(function($) {
 
 
 
-  console.log(masterPointArray)
-
   //createjs.Ticker.addEventListener("tick", handleTick);
   function handleTick(event) {
     lineShape.graphics.clear();
     lineShape.graphics.beginStroke("white");
 
     _.each(masterPointArray, function(idx, i){
-        masterPointArray[i].x += 1;
-        masterPointArray[i].y += 1;
+      _.each(masterPointArray[i], function(idxC, iC){
+      if(i == 0){
+        masterPointArray[i][iC].x += masterPointArray[i][iC].dirX;
+        masterPointArray[i][iC].y += masterPointArray[i][iC].dirY;
+      }else{
+        masterPointArray[i][iC].x = masterPointArray[0][iC].x;
+        masterPointArray[i][iC].y = masterPointArray[0][iC].y;
+      }
 
-        lineShape.graphics.lineTo(masterPointArray[i].x, masterPointArray[i].y);
-        lineShape.graphics.lineTo(masterPointArray[i].x, masterPointArray[i].y);
-        if(i == lineChildren - 1){
-          lineShape.graphics.lineTo(masterPointArray[0].x, masterPointArray[0].y);
-
+        if(masterPointArray[i][iC].x < 0){
+          masterPointArray[i][iC].dirX *= -1;
+          masterPointArray[i][iC].x *= -1;
+        }if(masterPointArray[i][iC].x > canvas.width){
+          masterPointArray[i][iC].dirX *= -1;
+        }if(masterPointArray[i][iC].y < 0){
+          masterPointArray[i][iC].dirY *= -1;
+        }if(masterPointArray[i][iC].y > canvas.height){
+          masterPointArray[i][iC].dirY *= -1;
         }
+        lineShape.graphics.lineTo(masterPointArray[i][iC].x + (i * degradeDistance), masterPointArray[i][iC].y + (i *degradeDistance) );
+        if(iC == lineChildren - 1){
+          lineShape.graphics.lineTo(masterPointArray[i][0].x + (i * degradeDistance), masterPointArray[i][0].y + (i *degradeDistance) );
+        }
+      });
+      var clonedArray = JSON.parse(JSON.stringify(childrenArray))
+      masterPointArray.push(clonedArray);
+      masterPointArray = masterPointArray.slice(0,lineChildren);
 
-
-
-        masterPointArray[i].XhistoryArray.push(masterPointArray[i].x);
-        masterPointArray[i].XhistoryArray = masterPointArray[i].XhistoryArray.slice(0,5);
-        masterPointArray[i].YhistoryArray.push(masterPointArray[i].y);
-        masterPointArray[i].YhistoryArray = masterPointArray[i].YhistoryArray.slice(0,5);
-    })
-
-
+    });
 
     stage.update();
 
