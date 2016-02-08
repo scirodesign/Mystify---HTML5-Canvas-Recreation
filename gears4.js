f4a8359d528b8aa9618c68ae0cfa9d42f5c4c38c
Andy <andy@scirodesign.com>
2016-02-08 03:26:22 -0500

    working demo

--- gears4.js
+++ gears4.js
@@ -10,20 +10,20 @@ jQuery(document).ready(function($) {
   var masterPointArray = [];
   createjs.Ticker.addEventListener("tick", handleTick);
   createjs.Ticker.setFPS(60);
-  canvas.width = jQuery(document).width();
-  canvas.height = jQuery(document).height();
+  canvas.width = jQuery(window).width();
+  canvas.height = jQuery(window).height();
+  jQuery(window).on("resize", resizeBrow);
   var phase = 1;
+  var lineShape = new createjs.Shape();
+  stage.addChild(lineShape);
 
-
+  //setup points for one line obj
   for(var i=0; i<pointsAmt; i++){
     var singleLinePoint = new pointObj(_.random(0, canvas.width),_.random(0, canvas.height),lineChildren);
     masterPointArray.push(singleLinePoint);
   }
 
-
-
-
-
+  //the point object
   function pointObj(px,py){
       this.x = px;
       this.y = py;
@@ -34,12 +34,7 @@ jQuery(document).ready(function($) {
       return this;
     }
 
-
-
-  var lineShape = new createjs.Shape();
-  stage.addChild(lineShape);
-
-
+  //sine color incrementer
   function colorText(phase)
   {
     var center = 128;
@@ -50,12 +45,17 @@ jQuery(document).ready(function($) {
     return red +"," + green  + "," + blue;
   }
 
+  //on browser resize change canvas dims
+  function resizeBrow(){
+    canvas.width = jQuery(window).width();
+    canvas.height = jQuery(window).height();
+  }
+
   function handleTick(event) {
     //increase phase gently for color change of sine
     phase+=.01;
 
     lineShape.graphics.clear();
-    console.log(colorText(phase));
     lineShape.graphics.beginStroke("rgb(" + colorText(phase) + ")");
 
     _.each(masterPointArray, function(idx, i){
@@ -71,7 +71,6 @@ jQuery(document).ready(function($) {
       masterPointArray[i].x += masterPointArray[i].dir[0];
       masterPointArray[i].y += masterPointArray[i].dir[1];
 
-
       //Screen Pos Logic
       if(masterPointArray[i].x < 0){
         masterPointArray[i].dir[0] *= -1;
@@ -83,16 +82,16 @@ jQuery(document).ready(function($) {
         masterPointArray[i].dir[1] *= -1;
       }
     });
-
     for(var lC = 0; lC < lineChildren; lC++){
-
       _.each(masterPointArray, function(idx, i){
-        if(i == 0) {
-          lineShape.graphics.moveTo(masterPointArray[i].ldir[lC].x + i, masterPointArray[i].ldir[lC].y + i);
-        }
-        lineShape.graphics.lineTo(masterPointArray[i].ldir[lC].x + i, masterPointArray[i].ldir[lC].y + i);
-        if(i == masterPointArray.length -1) {
-          lineShape.graphics.lineTo(masterPointArray[0].ldir[lC].x, masterPointArray[0].ldir[lC].y);
+        if(masterPointArray[0].ldir.length == lineChildren){
+          if(i === 0) {
+            lineShape.graphics.moveTo(masterPointArray[0].ldir[lC].x + i, masterPointArray[0].ldir[lC].y + i);
+          }
+          lineShape.graphics.lineTo(masterPointArray[i].ldir[lC].x + i, masterPointArray[i].ldir[lC].y + i);
+          if(i == masterPointArray.length -1) {
+            lineShape.graphics.lineTo(masterPointArray[0].ldir[lC].x, masterPointArray[0].ldir[lC].y);
+          }
         }
       });
     }
